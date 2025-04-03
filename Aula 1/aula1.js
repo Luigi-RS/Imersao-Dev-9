ff278be497541882a5e9c636b198d0d70f88fe04
Luigi-RS <luigi.segalla@gmail.com>
2025-04-03 00:04:08 -0300

    Implementacao da conversao em Dolar e Euro

--- Aula 1/aula1.js
+++ Aula 1/aula1.js
@@ -1,5 +1,20 @@
-function conversor() {
-    convwon = 0.0040
-    valorwons = prompt("Digite um valor em wons:")
-    alert("R$:" + valorwons * convwon)
+function conversorBrl() {
+    conversionRate = 0.003856
+    moneyIn = prompt("Digite um valor em wons:")
+    moneyOut = (moneyIn * conversionRate)
+    alert((moneyOut).toLocaleString("pt-BR",{style: "currency", currency: "BRL"}))
+}
+
+function conversorDol() {
+    conversionRate = 0.0006813
+    moneyIn = prompt("Digite um valor em wons:")
+    moneyOut = (moneyIn * conversionRate)
+    alert((moneyOut).toLocaleString("en-US",{style: "currency", currency: "USD"}))
+}
+
+function conversorEur() {
+    conversionRate = 0.0006241
+    moneyIn = prompt("Digite um valor em wons:")
+    moneyOut = (moneyIn * conversionRate)
+    alert((moneyOut).toLocaleString("it-IT",{style: "currency", currency: "EUR"}))
 }
